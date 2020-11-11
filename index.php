6d4874e597f896169c9584b6fd549457671b5135
nerissamaglente <nerissamaglente2@gmail.com>
2020-11-11 19:18:49 +0800

    add index for assign_two

--- index.php
+++ index.php
@@ -1,126 +1,60 @@
 <!DOCTYPE html>
-             <html lang="en" dir="ltr">
-               <head>
-                 <meta charset="utf-8">
-                 <title></title>
-               </head>
-     <body>
-     </select>
-       <div class="container">
-             <div class="row">
-                <div class="col-6">
-                <h2><?php echo "Nerissa C. Maglente";?></h4>
-            </div>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
 
-           <form id="calculator" class="" action="" method="post">
-         <div class="col-4">
-           <h3>CALCULATOR</h3>
-           <p>Input First Number:</p>
-           <br>
-           <input type="number" name="input1" placeholder="" required >
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <?php
+            class User{
+              public $name;
+              public $username;
+              public $email;
 
-           <br><br>
-           <p>Input Second Number:</p>
-           <br>
-           <input type="number" name="input2" placeholder="" required>
-           <br><br>
+            function set_name($name){
+              $this->name = $name;
+            }
+            function get_name(){
+              return $this->name;
+            }
+            function set_username($username){
+              $this->username = $username;
+            }
+            function get_username(){
+              return $this->username;
+            }
+            function set_email($email){
+              $this->email = $email;
+            }
+            function get_email(){
+              return $this->email;
+            }
 
-           <button type="submit" name="add" value="add">+</button>
-           <button type="submit" name="subtract" value="subtract">-</button>
-           <button type="submit" name="multiplication" value="multiplication">*</button>
-           <button type="submit" name="division" value="division">/</button>
-           <br>
-           </form>
-           <p>Answer</p>
-                       <type="text"  name="name"  value="<?php echo @$res;?>">
-                         <?php
-                             if (isset($_POST['add'])) {
-                               $num1 = $_POST['input1'];
-                               $num2 = $_POST['input2'];
-                               $res = $num1 + $num2;
-                               echo $res;
-                             }
-                             if (isset($_POST['subtract'])) {
-                               $num1 = $_POST['input1'];
-                               $num2 = $_POST['input2'];
-                               $res = $num1 - $num2;
-                               echo $res;
-                             }
-                             if (isset($_POST['multiplication'])) {
-                               $num1 = $_POST['input1'];
-                               $num2 = $_POST['input2'];
-                               $res = $num1 * $num2;
-                               echo $res;
-                             }
-                             if (isset($_POST['divide'])) {
-                               $num1 = $_POST['input1'];
-                               $num2 = $_POST['input2'];
-                               $res = $num1 / $num2;
-                               echo $res;
-                             }
+            function addFriend($name){
+              echo $name . " added a friend <br>";
+            }
+            function postStatus($name){
+              echo $name . " posted a status <br>";
+            }
+          }
 
-                            ?>
+            $example = new User();
+            $example->set_name("Blance");
+            $example->set_email("Blance@gmail.com");
+            $example->set_username("Blance123");
 
-                   </div>
+            echo "Name: " . $example->get_name() . "<br>";
+            echo "Username: " . $example->get_username() . "<br>";
+            echo "Email: " . $example->get_email() . "<br>";
+            $example->addFriend("Blance");
+            $example->postStatus("Blance");
+     ?>
 
 
-           <form id="GCD" class="" action="" method="post" >
-         <div class="col-4">
-           <h3>FINDING GCD</h3>
-           <p>Input First Number:</p>
-           <br>
-           <input type="number" name="gcd1" placeholder="" required>
 
-           <br><br>
-           <p>Input Second Number:</p>
-           <br>
-           <input type="number" name="gcd2"placeholder="" required>
-           <br><br>
-           <button type="submit" name="gcd">Submit</button>
-           </form>
-                      <p>Answer</p>
-
-                           <?php
-                         if(isset($_POST['gcd']))
-                         {
-                           $gcd1 = $_POST['gcd1'];
-                           $gcd2 = $_POST['gcd2'];
-                           $gcd = $_POST['gcd'];
-
-                           if($gcd1 > $gcd2)
-                           {
-                             $temp = $gcd1;
-                             $gcd1 = $gcd2;
-                             $gcd2 = $temp;
-                           }
-                           for($i = 1; $i < ($gcd1 + 1); $i++ )
-                           {
-                             if ($gcd1%$i == 0 and $gcd2%$i == 0)
-                             $gcd = $i;
-                           }
-
-                           echo "GCD of $gcd1 and $gcd2 is: $gcd";
-                         }
-                       ?>
-
-         </div>
-       </div>
-     </div>
-
-     <style media="screen">
-           h2
-           {
-             position: relative; left: 100px; top: 190px;
-           }
-           #calculator
-           {
-             position: relative; left: 400px; top: 105px;
-           }
-           #GCD
-           {
-             position: relative; left: 700px; top: -210px;
-           }
-         </style>
-
-</body>
+  </body>
 </html>
